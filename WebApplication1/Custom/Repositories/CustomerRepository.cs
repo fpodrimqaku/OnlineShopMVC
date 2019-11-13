94db481bd81e0f4fcbe34a4fef442a826c2d41e3
Faton Podrimçaku <faton.podrimcaku@company.domain>
2019-11-13 15:35:10 +0100

    initial crud

--- WebApplication1/Custom/Repositories/CustomerRepository.cs
+++ WebApplication1/Custom/Repositories/CustomerRepository.cs
@@ -10,14 +10,41 @@ namespace WebApplication1.Custom.Repositories
 {
     public class CustomerRepository
     {
+        public async Task<DatabaseExecuteResult> InsertCustomer(Customer customer)
+        {
+            return (await new Query().Execute("EXEC CustomerCrud @operation = 0,@name", new { customer.Name }));
+        }
 
-        public async Task<DatabaseResult<Customer>> getAllCustomers() {
+
+        public async Task<DatabaseResult<Customer>> getAllCustomers()
+        {
             return (await new Query().Select<Customer>("EXEC CustomerCrud @operation = 1"));
         }
-        
 
 
-
+        public async Task<DatabaseExecuteResult> UpdateCustomer(Customer customer)
+        {
+            return (await new Query().Execute("EXEC CustomerCrud @operation = 2,@id,@Name", new
+            {
+                customer.Id,
+                customer.Name
+                
+            }));
         }
+
+        public async Task<DatabaseExecuteResult> DeleteCustomer(int id)
+        {
+            return (await new Query().Execute("EXEC CustomerCrud @operation = 3,@id", new { id }));
+        }
+
+
+
+
+
+
+
+
+
+    }
 }
 
